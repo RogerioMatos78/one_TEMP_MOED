49b44f8cfa313caeee8ff33831de02d3f05ff12e
RogerioMatos78 <rogerio.matos78@gmail.com>
2023-02-22 19:41:54 -0300

    2 teste

--- src/oneTempModePck/executaTM.java
+++ src/oneTempModePck/executaTM.java
@@ -5,6 +5,7 @@ public class executaTM {
 	public static void main(String[] args) {
 		// Principal para Executar Concesor
 		System.out.println("teste de integração");
+		System.out.println("2teste de integração");
 
 	}
 

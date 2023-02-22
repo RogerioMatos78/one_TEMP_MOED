42fc7647409db1c24b0bbce930cfaa6d0cf1ff28
RogerioMatos78 <rogerio.matos78@gmail.com>
2023-02-22 19:48:52 -0300

    inicio da codificação

--- src/oneTempModePck/executaTM.java
+++ src/oneTempModePck/executaTM.java
@@ -4,9 +4,7 @@ public class executaTM {
 
 	public static void main(String[] args) {
 		// Principal para Executar Concesor
-		System.out.println("teste de integração");
-		System.out.println("2teste de integração");
-		System.out.println("3teste de integração desktop eclipse");
+
 
 	}
 
